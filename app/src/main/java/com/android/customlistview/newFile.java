2dbe47dc9bcbb0e3696c431944e30387010ff574
Naieem-55 <naieemislam27@gmail.com>
2023-03-23 02:44:20 +0600

    Changes Done

---
+++ app/src/main/java/com/android/customlistview/newFile.java
@@ -0,0 +1,30 @@
+package com.android.customlistview;
+
+public class newFile {
+    protected int x,y;
+
+    public newFile(){
+
+    }
+
+    public newFile(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}

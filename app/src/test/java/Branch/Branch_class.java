4aba0cdea70028e1dc0a86d1885abce42c5fb819
Naieem-55 <naieemislam27@gmail.com>
2023-03-23 23:54:19 +0600

    Modified branch partner from partner2

--- app/src/test/java/Branch/Branch_class.java
+++ app/src/test/java/Branch/Branch_class.java
@@ -4,15 +4,17 @@ public class Branch_class {
 
     String name,id;
     float cgpa;
+    double avg;
 
     Branch_class(){
 
     }
 
-    Branch_class(String a,String b,float c){
+    Branch_class(String a,String b,float c,double d){
         name=a;
         id=b;
         cgpa=c;
+        avg=d;
     }
 
     public String getName() {
@@ -37,5 +39,13 @@ public class Branch_class {
 
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
     }
 }
